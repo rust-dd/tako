aff7eabb626e270dc0c2fda2f5e4f07e82ad14e5
Daniel Boros <dancixx@gmail.com>
2025-07-11 01:27:34 +0200

    fix: jwt_auth middleware

--- src/middleware/jwt_auth.rs
+++ src/middleware/jwt_auth.rs
@@ -1,6 +1,7 @@
 use std::{collections::HashMap, future::Future, pin::Pin, sync::Arc};
 
 use http::{StatusCode, header::AUTHORIZATION};
+use jwt_simple::prelude::*;
 use serde::de::DeserializeOwned;
 
 use crate::{
@@ -9,24 +10,94 @@ use crate::{
     types::{Request, Response},
 };
 
+pub enum AnyVerifyKey {
+    HS256(Arc<HS256Key>),
+    HS384(Arc<HS384Key>),
+    HS512(Arc<HS512Key>),
+    Blake2b(Arc<Blake2bKey>),
+
+    RS256(Arc<RS256PublicKey>),
+    RS384(Arc<RS384PublicKey>),
+    RS512(Arc<RS512PublicKey>),
+
+    PS256(Arc<PS256PublicKey>),
+    PS384(Arc<PS384PublicKey>),
+    PS512(Arc<PS512PublicKey>),
+
+    ES256(Arc<ES256PublicKey>),
+    ES256K(Arc<ES256kPublicKey>),
+    ES384(Arc<ES384PublicKey>),
+
+    EdDSA(Arc<Ed25519PublicKey>),
+}
+
+impl AnyVerifyKey {
+    pub fn alg_id(&self) -> &'static str {
+        match self {
+            Self::HS256(_) => "HS256",
+            Self::HS384(_) => "HS384",
+            Self::HS512(_) => "HS512",
+            Self::Blake2b(_) => "BLAKE2B",
+
+            Self::RS256(_) => "RS256",
+            Self::RS384(_) => "RS384",
+            Self::RS512(_) => "RS512",
+
+            Self::PS256(_) => "PS256",
+            Self::PS384(_) => "PS384",
+            Self::PS512(_) => "PS512",
+
+            Self::ES256(_) => "ES256",
+            Self::ES256K(_) => "ES256K",
+            Self::ES384(_) => "ES384",
+
+            Self::EdDSA(_) => "EdDSA",
+        }
+    }
+
+    pub fn verify<C>(&self, token: &str) -> Result<JWTClaims<C>, jwt_simple::Error>
+    where
+        C: Serialize + DeserializeOwned,
+    {
+        let opts = VerificationOptions::default();
+        match self {
+            Self::HS256(k) => k.verify_token::<C>(token, Some(opts)),
+            Self::HS384(k) => k.verify_token::<C>(token, Some(opts)),
+            Self::HS512(k) => k.verify_token::<C>(token, Some(opts)),
+            Self::Blake2b(k) => k.verify_token::<C>(token, Some(opts)),
+
+            Self::RS256(k) => k.verify_token::<C>(token, Some(opts)),
+            Self::RS384(k) => k.verify_token::<C>(token, Some(opts)),
+            Self::RS512(k) => k.verify_token::<C>(token, Some(opts)),
+
+            Self::PS256(k) => k.verify_token::<C>(token, Some(opts)),
+            Self::PS384(k) => k.verify_token::<C>(token, Some(opts)),
+            Self::PS512(k) => k.verify_token::<C>(token, Some(opts)),
+
+            Self::ES256(k) => k.verify_token::<C>(token, Some(opts)),
+            Self::ES256K(k) => k.verify_token::<C>(token, Some(opts)),
+            Self::ES384(k) => k.verify_token::<C>(token, Some(opts)),
+
+            Self::EdDSA(k) => k.verify_token::<C>(token, Some(opts)),
+        }
+    }
+}
+
 pub struct JwtAuth<T>
 where
-    T: Clone + DeserializeOwned + Send + Sync,
+    T: DeserializeOwned + Send + Sync + 'static,
 {
-    keys: Arc<HashMap<Algorithm, DecodingKey>>,
-    allowed_algos: Vec<Algorithm>,
+    keys: Arc<HashMap<&'static str, AnyVerifyKey>>,
     _phantom: std::marker::PhantomData<T>,
 }
 
 impl<T> JwtAuth<T>
 where
-    T: Clone + DeserializeOwned + Send + Sync,
+    T: DeserializeOwned + Send + Sync + 'static,
 {
-    pub fn new(keys: HashMap<Algorithm, DecodingKey>) -> Self {
-        let allowed_algos = keys.keys().cloned().collect();
+    pub fn new(keys: HashMap<&'static str, AnyVerifyKey>) -> Self {
         Self {
             keys: Arc::new(keys),
-            allowed_algos,
             _phantom: std::marker::PhantomData,
         }
     }
@@ -34,7 +105,7 @@ where
 
 impl<T> IntoMiddleware for JwtAuth<T>
 where
-    T: Clone + DeserializeOwned + Send + Sync + 'static,
+    T: Clone + Serialize + DeserializeOwned + Send + Sync + 'static,
 {
     fn into_middleware(
         self,
@@ -44,21 +115,18 @@ where
     + Sync
     + 'static {
         let keys = self.keys.clone();
-        let allowed_algos = self.allowed_algos.clone();
 
         move |mut req: Request, next: Next| {
             let keys = keys.clone();
-            let allowed_algos = allowed_algos.clone();
 
             Box::pin(async move {
-                let token_opt = req
+                let token = match req
                     .headers()
                     .get(AUTHORIZATION)
                     .and_then(|v| v.to_str().ok())
                     .and_then(|s| s.strip_prefix("Bearer "))
-                    .map(str::trim);
-
-                let token = match token_opt {
+                    .map(str::trim)
+                {
                     Some(t) => t,
                     None => {
                         return (
@@ -69,47 +137,35 @@ where
                     }
                 };
 
-                let header = match decode_header(token) {
+                let token_meta = match jwt_simple::token::Token::decode_metadata(token) {
                     Ok(h) => h,
                     Err(_) => {
-                        return (StatusCode::UNAUTHORIZED, "Invalid JWT header").into_response();
+                        return (StatusCode::UNAUTHORIZED, "Cannot decode JWT header")
+                            .into_response();
                     }
                 };
 
-                let alg = header.alg;
-                if !allowed_algos.contains(&alg) {
-                    return (
-                        StatusCode::UNAUTHORIZED,
-                        format!("Algorithm {:?} not allowed", alg),
-                    )
-                        .into_response();
-                }
-
-                let key = match keys.get(&alg) {
+                let alg = &token_meta.algorithm();
+                let verify_key = match keys.get(alg) {
                     Some(k) => k,
                     None => {
                         return (
                             StatusCode::UNAUTHORIZED,
-                            format!("No key for algorithm {:?}", alg),
+                            format!("Algorithm {} not allowed", alg),
                         )
                             .into_response();
                     }
                 };
 
-                let mut validation = Validation::new(alg);
-                validation.algorithms = allowed_algos.clone();
-
-                let decoded = decode::<T>(token, key, &validation);
-
-                let token_data = match decoded {
-                    Ok(data) => data,
-                    Err(err) => {
-                        return (StatusCode::UNAUTHORIZED, format!("Invalid token: {}", err))
+                let claims = match verify_key.verify::<T>(token) {
+                    Ok(c) => c,
+                    Err(e) => {
+                        return (StatusCode::UNAUTHORIZED, format!("Invalid token: {}", e))
                             .into_response();
                     }
                 };
 
-                req.extensions_mut().insert(token_data.claims);
+                req.extensions_mut().insert(claims);
                 next.run(req).await.into_response()
             })
         }
