8ff44d397e0f424ee203fdda499a2f5b3545fb5f
Daniel Boros <dancixx@gmail.com>
2025-07-10 18:06:19 +0200

    feat: add docs to form

--- src/extractors/form.rs
+++ src/extractors/form.rs
@@ -5,12 +5,28 @@ use serde::de::DeserializeOwned;
 
 use crate::{extractors::AsyncFromRequestMut, types::Request};
 
+/// Represents a form extracted from an HTTP request body.
+///
+/// This generic struct wraps the deserialized form data of type `T`.
 pub struct Form<T>(pub T);
 
+/// Implementation of the `AsyncFromRequestMut` trait for extracting form data from an HTTP request.
+///
+/// This implementation supports asynchronous extraction of form data from requests with the
+/// `application/x-www-form-urlencoded` content type. The extracted data is deserialized into
+/// the generic type `T`.
 impl<'a, T> AsyncFromRequestMut<'a> for Form<T>
 where
     T: DeserializeOwned + Send,
 {
+    /// Extracts form data from the HTTP request body.
+    ///
+    /// # Arguments
+    /// * `req` - A mutable reference to the HTTP request from which the form data is extracted.
+    ///
+    /// # Returns
+    /// * `Ok(Form<T>)` - If the request contains valid form data that can be deserialized into type `T`.
+    /// * `Err` - If the content type is invalid or the form data cannot be parsed or deserialized.
     async fn from_request(req: &'a mut Request) -> anyhow::Result<Self> {
         if req
             .headers()
