755a0fdfac52b2109df4ab9b3a8e3515c9e2b8b7
Daniel Boros <dancixx@gmail.com>
2025-12-02 00:54:21 +0100

    feat: add upload progress hub

--- src/plugins/upload_progress.rs
+++ src/plugins/upload_progress.rs
@@ -1,10 +1,51 @@
+use std::sync::Arc;
+
+use dashmap::DashMap;
 use http::Method;
+use tokio::sync::broadcast;
+use uuid::Uuid;
 
 use crate::plugins::TakoPlugin;
 
 #[derive(Clone)]
 pub struct Config<'a> {
   pub path: &'a str,
+}
+
+#[derive(Clone)]
+pub struct UploadProgress {
+  pub id: Uuid,
+  pub bytes: u64,
+  pub total: Option<u64>,
+}
+
+#[derive(Clone)]
+pub struct UploadProgressHub {
+  inner: Arc<DashMap<Uuid, broadcast::Sender<UploadProgress>>>,
+}
+
+impl UploadProgressHub {
+  pub fn new() -> Self {
+    Self {
+      inner: Arc::new(DashMap::new()),
+    }
+  }
+
+  pub fn register(&self, id: Uuid) -> broadcast::Receiver<UploadProgress> {
+    let (tx, rx) = broadcast::channel(100);
+    self.inner.insert(id, tx);
+    rx
+  }
+
+  pub fn subscribe(&self, id: &Uuid) -> Option<broadcast::Receiver<UploadProgress>> {
+    self.inner.get(&id).map(|e| e.value().subscribe())
+  }
+
+  pub fn notify(&self, msg: UploadProgress) {
+    if let Some(tx) = self.inner.get(&msg.id) {
+      let _ = tx.send(msg);
+    }
+  }
 }
 
 pub struct UploadProgressPlugin<'a>(Config<'a>);
